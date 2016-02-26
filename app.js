bdf3ac295e76c763fe6f9531c8b12d1cd353422f
Rick Budzak <pyroja@gmail.com>
2016-02-26 09:35:58 -0800

    add window.onload, fix div ghosting

--- app.js
+++ app.js
@@ -1,83 +1,87 @@
-console.log("Javascript is alive!");
 
-var greeting = document.querySelector('#greeting');
+window.onload  = function(e){
+  
+  console.log("Javascript is alive!");
 
-var imgFood = document.querySelector('img');
+  var greeting = document.querySelector('#greeting');
 
-greeting.innerText = "Hello, World!";
+  var imgFood = document.querySelector('img');
 
-var allLists = document.getElementsByTagName('li');
+  greeting.innerText = "Hello, World!";
 
-for (i = 0;i < allLists.length; i++){
-  allLists[i].style.backgroundColor = "yellow";
-}
+  var allLists = document.getElementsByTagName('li');
 
-var divGreeting = document.querySelector('#greeting');
-var imgTumblr = document.createElement('img');
-imgTumblr.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
-divGreeting.appendChild(imgTumblr);
-
-document.getElementById('essentials').addEventListener("click", function(){
   for (i = 0;i < allLists.length; i++){
-    allLists[i].className = "";
+    allLists[i].style.backgroundColor = "yellow";
   }
-  event.target.className = "selected";
-  var imagePath = "images/" + event.target.innerText + ".jpeg";
-  imgFood.src = imagePath;
-});
 
-var divGhost=document.getElementById("ghosting");
-function hide(e) {
-  e.target.style.visibility = 'hidden';
-}
-divGhost.addEventListener('mouseenter', hide);
+  var divGreeting = document.querySelector('#greeting');
+  var imgTumblr = document.createElement('img');
+  imgTumblr.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
+  divGreeting.appendChild(imgTumblr);
 
-var divSize=document.getElementById("resize");
-function resize(e) {
-  var divWidth = e.target.offsetWidth;
-  e.target.style.width = (divWidth * 2)+"px";
-}
-divSize.addEventListener('mouseenter', resize);
-
-function origSize(e) {
-  var divWidth = e.target.offsetWidth;
-  e.target.style.width = (divWidth / 2)+"px";
-}
-divSize.addEventListener('mouseleave', origSize);
-
-function panic(e) {
-  for (i = 0;i < allLists.length; i++){
-    allLists[i].className = "";
-  }
-  imgFood.src = "images/panic.jpeg";
-}
-divSize.addEventListener('mouseenter', resize);
-document.querySelector('input').addEventListener('click', panic);
-
-function numbers(charCode, e) {
-  if (charCode > 47 && charCode < 58){
-    alert("I HATE NUMBERZZZ!");
-  }
-}
-
-var arrKonCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-function konamiCode(charCode, arrKonami, e) {
-  if (charCode === arrKonami[0]){
-    arrKonami.shift();
-    if (arrKonami.length === 0){
-      alert('YOU ARE AN EVENT HANDLER GURUUUUUUUUU!');
-      return [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  document.getElementById('essentials').addEventListener("click", function(){
+    for (i = 0;i < allLists.length; i++){
+      allLists[i].className = "";
     }
-    else{
+    event.target.className = "selected";
+    var imagePath = "images/" + event.target.innerText + ".jpeg";
+    imgFood.src = imagePath;
+  });
+
+  var divGhost=document.getElementById("ghosting");
+  function hide(e) {
+    e.target.style.display = 'none';
+  }
+  divGhost.addEventListener('mouseenter', hide);
+
+  var divSize=document.getElementById("resize");
+  function resize(e) {
+    var divWidth = e.target.offsetWidth;
+    e.target.style.width = (divWidth * 2)+"px";
+  }
+  divSize.addEventListener('mouseenter', resize);
+
+  function origSize(e) {
+    var divWidth = e.target.offsetWidth;
+    e.target.style.width = (divWidth / 2)+"px";
+  }
+  divSize.addEventListener('mouseleave', origSize);
+
+  function panic(e) {
+    for (i = 0;i < allLists.length; i++){
+      allLists[i].className = "";
+    }
+    imgFood.src = "images/panic.jpeg";
+  }
+  divSize.addEventListener('mouseenter', resize);
+  document.querySelector('input').addEventListener('click', panic);
+
+  function numbers(charCode, e) {
+    if (charCode > 47 && charCode < 58){
+      alert("I HATE NUMBERZZZ!");
+    }
+  }
+
+  var arrKonCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  function konamiCode(charCode, arrKonami, e) {
+    if (charCode === arrKonami[0]){
+      arrKonami.shift();
+      if (arrKonami.length === 0){
+        alert('YOU ARE AN EVENT HANDLER GURUUUUUUUUU!');
+        return [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+      }
+      else{
       
-      return arrKonami;
+        return arrKonami;
+      }
     }
+    return [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
   }
-  return [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-}
   
 
-document.addEventListener('keydown', function(e){ var charCode = event.keyCode;
-      numbers(charCode);
-      arrKonCode = konamiCode(charCode, arrKonCode);
-    });
+  document.addEventListener('keydown', function(e){ var charCode = event.keyCode;
+        numbers(charCode);
+        arrKonCode = konamiCode(charCode, arrKonCode);
+      });
+};
