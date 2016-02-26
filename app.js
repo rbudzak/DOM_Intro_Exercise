7cbb2b1dff748e5d22bccb26acf3c06ad0b47a09
Rick Budzak <pyroja@gmail.com>
2016-02-25 18:02:02 -0800

    up to adding the gif

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Javascript is alive!");
+
+var greeting = document.querySelector('#greeting');
+
+greeting.innerText = "Hello, World!";
+
+var allLists = document.getElementsByTagName('li');
+
+for (i = 0;i < allLists.length; i++){
+  allLists[i].style.backgroundColor = "yellow";
+}
+
+var divGreeting = document.querySelector('#greeting');
+var imgTumblr = document.createElement('img');
+imgTumblr.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
+divGreeting.appendChild(imgTumblr);
+
