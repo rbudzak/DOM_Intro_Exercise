1e4186c1e1576f901f8109afc6492599ec8d727a
Rick Budzak <pyroja@gmail.com>
2016-02-26 00:52:40 -0800

    add all but the last required solution

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ console.log("Javascript is alive!");
 
 var greeting = document.querySelector('#greeting');
 
+var imgFood = document.querySelector('img');
+
 greeting.innerText = "Hello, World!";
 
 var allLists = document.getElementsByTagName('li');
@@ -15,3 +17,39 @@ var imgTumblr = document.createElement('img');
 imgTumblr.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
 divGreeting.appendChild(imgTumblr);
 
+document.getElementById('essentials').addEventListener("click", function(){
+  for (i = 0;i < allLists.length; i++){
+    allLists[i].className = "";
+  }
+  event.target.className = "selected";
+  var imagePath = "images/" + event.target.innerText + ".jpeg";
+  imgFood.src = imagePath;
+});
+
+var divGhost=document.getElementById("ghosting");
+function hide(e) {
+  e.target.style.visibility = 'hidden';
+}
+divGhost.addEventListener('mouseenter', hide);
+
+var divSize=document.getElementById("resize");
+function resize(e) {
+  var divWidth = e.target.offsetWidth;
+  e.target.style.width = (divWidth * 2)+"px";
+}
+divSize.addEventListener('mouseenter', resize);
+
+function origSize(e) {
+  var divWidth = e.target.offsetWidth;
+  e.target.style.width = (divWidth / 2)+"px";
+}
+divSize.addEventListener('mouseleave', origSize);
+
+function panic(e) {
+  for (i = 0;i < allLists.length; i++){
+    allLists[i].className = "";
+  }
+  imgFood.src = "images/panic.jpeg";
+}
+divSize.addEventListener('mouseenter', resize);
+document.querySelector('input').addEventListener('click', panic);
